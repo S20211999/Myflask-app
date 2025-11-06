7501f15c0f246c749aeba5ea141291ad616a6bb9
S20211999 <82024162+S20211999@users.noreply.github.com>
2025-11-06 09:40:27 +0530

    Add Google service account configuration
    Added service account configuration for Google API access.

--- quick_fix.py
+++ quick_fix.py
@@ -1,1 +1,14 @@
 
+{
+  "type": "service_account",
+  "project_id": "online-exam-477315",
+  "private_key_id": "2cbccea77532f5c36cdfc5d6b3b20aa0b341df15",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDBA6XrgUlPrQGv\nC2308beLDWarOJil92u1Pa/x3IGGAn00TaeX7rctqPR0f3evynyslTWSxVuUzTni\nmZOVyetlR7J497rvQaCmXgJw7JhULArGzsVhtnFTgxCVJiCa65xM5HrDVIc5ITdD\nkC8otXFLZwNefCRuhjVyfSYDXVzfTpqrwnW6+d2z/4Zol43zJdEktpnZbfOzmBL5\no0LuKtpVqSF/w25MOlkDFMp6edcGLPFuQV3YFOzLRr4yYGV3NJPl4SmmA4hNGFWM\nzy1P4a8dFl67rLU1/zgZYYt23H+2K+VQ29Htf//jcgqJUPwP5yBGOEDF8fQ46VTv\nMazv3Aj3AgMBAAECggEARbR/k1GZnECqvwCtsSwqUPmB/ovt9OIp78ZbaEluLWtw\ntBbnU6NOXBsMTZZLLx09cUZhlqPXsyF6qi80cX/+lxERj0ZIKGfXEL4au5V2Xc+a\nBLpf5i3xN2u57WNbmxdD1zEcdLZuL1+jia7kmRsN02Fcgh7rOU8WtkYkGEr7Y1lq\ni/JJmGb0cv8gsSyyGf8EiwXf9u/S7vhKot3DDtx5KRNrV4pdDHiXGejr4d3kVYEg\nDGbToz0ihl4uRB51FykDPvJAZARmTiuaxnQVOCR7yhyD3IASelbp6FAJK9rIAyV8\n234am80oCDaTt4b9PT5N97/yWFB77blAqgpCikgCYQKBgQD02D3zz/hQmqWhUFPt\nG2+QnW+uzYTBfRS45Kx//VTRFOZFjfXG2U2BZQZKunlacC7pvX3pEBZovJHQNdqG\nNxGU3AZiAQp2Exqxy9x7+7Z2nHxWA5iYxqfJDnWWmylph8Q9DS5yT6xTVThRDcOE\ntJJumxgK4CNQUw9OTLPuIz86PQKBgQDJzuEUCeQ1SvsasnI7krEeiw7mlNjzk/ln\nuDL3Pdpn222JJXGJQy+a46uIPjsAKWyZWQiuFdHj8D2N3p5vANEn4ZoP9ZFAwIZY\n7Qwt7hZ/V7Z7MA24CJ1vIzc4ySOp/1wE11o9BuulDk4wkaG/0sp4aAtKciD4oWS1\nTcolM7qnQwKBgHcbTp/xlqFXaaVcl0cr5ozaGX9WmPMrLs5253dnvD/tw8soMNyn\nzy1YAMSPjAMzuY6ZUMv0uIIA4YoA+Kn71gCtPCmmyfkAeCbvvfVOUMfIfi+1liET\n2oFTGF7IYk/k7NCqPeL74R+0wC2l3WwMD4PgzMRVnrgvf62eYJVC7jodAoGBAJwS\nPse8jp0Yi/Itp2KjJA+3A6MX4mEeQ3gPYHsNfudrL98s8g1xxFNJV9NaLU8GulIu\nJSRdA6IAndomHOlD/H2XYAqPo/Ir8rq7OPwccZGySBIlK7b0Eq1s0rw78jT6pDOq\nSMOBoqRX1+h/+FPdtXBBJtpAQXeHedctffQfeW75AoGBAOXV/sDOwDSTTDDx7fpx\nRwvkyQsSz5L0EhAlwX3IqnQ4bvCM57JPViBDE6TS3GRPJcRiBIVBI5zrwfg/A7jx\nUOIBVR3qVjebIB0enNA8TxDA5Qipt0EjohB//zSFVu3ptCW6oDFR7oYUDm6r6Eto\nI+kyJRGy9R8uMdj04zzDOaF5\n-----END PRIVATE KEY-----\n",
+  "client_email": "sienna-online-exam@online-exam-477315.iam.gserviceaccount.com",
+  "client_id": "106564040827760533139",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/sienna-online-exam%40online-exam-477315.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
